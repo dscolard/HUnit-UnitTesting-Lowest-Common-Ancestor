2f81d02b55c9d938ba1e81a3a784b2ef41624d42
David Scolard <davidscolard@Davids-MacBook-Pro.local>
2018-10-01 17:48:52 +0100

    tests

--- test/LCA.hs
+++ test/LCA.hs
@@ -5,6 +5,8 @@ module LCA
     ( myTree, lca, lca_show
     ) where
 
+import Text.Printf
+
 
 
 data Tree a = Empty | Node a (Tree a) (Tree a) deriving Show
@@ -12,20 +14,20 @@ data Tree a = Empty | Node a (Tree a) (Tree a) deriving Show
 --Create tree
 myTree :: Tree Int
 myTree = Node 1 
-	    (Node 2 
-	        (Node 4 Empty Empty)
-	        (Node 5 Empty Empty)
-	    )
-	    (Node 3 
-	        (Node 6 
-	            (Node 10 Empty Empty) 
-	            (Node 8 Empty Empty)
-	        )
-	        (Node 7 
-	            (Node 9 Empty Empty) 
-	            Empty
-	        )
-	    )
+        (Node 2 
+            (Node 4 Empty Empty)
+            (Node 5 Empty Empty)
+        )
+        (Node 3 
+            (Node 6 
+                (Node 10 Empty Empty) 
+                (Node 8 Empty Empty)
+            )
+            (Node 7 
+                (Node 9 Empty Empty) 
+                Empty
+            )
+        )
 
 --Solve for common ancestor
 lca :: Eq a => Tree a -> a -> a -> Either Bool a
@@ -46,14 +48,14 @@ lca (Node v tl tr) n1 n2 =
         _ -> Left False
 
 lca_show :: (Eq a, Show a, PrintfArg a) => Tree a -> a -> a -> String
-lca_show t n1 n2 = printf "LCA(%d,%d)=%s" n1 n2 result
+lca_show t n1 n2 = printf "LCA(%d,%d) = %s" n1 n2 result
     where result = case lca t n1 n2 of
                         Right a -> show a
                         _ -> "not found"
 
 
 
-{--Test for examples
+{-
 main = mapM print [
             lca_show myTree 0 9, --Expected: "not found"
             lca_show myTree 0 5, --Expected: "not found"
---
+++ test/LcaSpec.hs
@@ -0,0 +1,30 @@
+module LcaSpec where
+
+import   LCA
+import   Test.Hspec
+import   Test.QuickCheck
+
+main :: IO ()
+main = hspec $ do
+  describe "Lowest Common Ancestor Function" $ do
+
+   it "returns 'not found' for 0 9" $ do
+      lca_show myTree 0 9 `shouldBe` "LCA(0,9) = not found"
+
+   it "returns 'not found' for 0 5" $ do
+      lca_show myTree 0 5 `shouldBe` "LCA(0,5) = not found"
+
+   it "returns 'not found' for 4 5" $ do
+      lca_show myTree 4 5 `shouldBe` "LCA(4,5) = 2"
+
+   it "returns 'not found' for 4 6" $ do
+      lca_show myTree 4 6 `shouldBe` "LCA(4,6) = 1"
+
+   it "returns 'not found' for 3 4" $ do
+      lca_show myTree 3 4 `shouldBe` "LCA(3,4) = 1"
+
+   it "returns 'not found' for 2 4" $ do
+      lca_show myTree 0 9 `shouldBe` "LCA(2,4) = not found"
+
+   it "returns 'not found' for 0 9" $ do
+      lca_show myTree 0 9 `shouldBe` "LCA(0,9) = not found"
